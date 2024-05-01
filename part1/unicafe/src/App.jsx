5a77a306dcbf03ef9bbdd53e6c865316f87c0a5c
atunio1 <atunio@seas.upenn.edu>
2024-05-01 16:13:46 -0400

    Completed exercises 1.6-1.11 for unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,26 +1,68 @@
 import { useState } from 'react'
 
+const StatisticLine = (props) => {
+  return (
+    <tr>
+    <td>{props.text}</td> 
+    <td>{props.value}</td>
+    </tr>
+  )
+}
+
+const Statistics = (props) => {
+  const good = props.good
+  const bad = props.bad
+  const neutral = props.neutral
+  const all = props.all
+
+  return (
+      <table>
+        <tbody>
+          <StatisticLine text="good" value ={good} />
+          <StatisticLine text="neutral" value ={neutral} />
+          <StatisticLine text="bad" value ={bad} />
+          <StatisticLine text="all" value ={all} />
+          <StatisticLine text="average" value ={(((good  * 1) + (neutral * 0) + (bad * -1)) / all)} />
+          <StatisticLine text="positive" value ={((good) / all) * 100 + '%'} />
+        </tbody>
+      </table>
+  )
+}
+
+const Button = (props) => {
+  return (
+  <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
-  const increaseCount = (type, setType) => {
+  const increaseCount = (type, all, setType) => {
     const updatedCount = type + 1
+    const updatedAll = all + 1
     setType(updatedCount)
+    setAll(updatedAll)
   }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => increaseCount(good, setGood)}>good</button>
-      <button onClick={() => increaseCount(neutral, setNeutral)}>neutral</button>
-      <button onClick={() => increaseCount(bad, setBad)}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Button handleClick={() => increaseCount(good, all, setGood)} text="good" />
+      <Button handleClick={() => increaseCount(neutral, all, setNeutral)} text="neutral" />
+      <Button handleClick={() => increaseCount(bad, all, setBad)} text="bad" />
+      <h1>Statistics</h1>
+      {all > 0 ? (
+        <Statistics good={good} bad={bad} neutral={neutral} all={all}/>
+      )
+      : (
+       <p>No feedback given</p> 
+      )
+      }
     </div>
   )
 }
